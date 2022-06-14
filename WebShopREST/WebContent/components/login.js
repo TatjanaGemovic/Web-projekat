b046465c78ab94e7341713a6c8f9c25cd9211baa
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2022-06-15 00:57:21 +0200

    Change

--- WebShopREST/WebContent/components/login.js
+++ WebShopREST/WebContent/components/login.js
@@ -10,8 +10,8 @@ Vue.component("login", {
 <div>
 	<form>
 		<table>
-			<tr><td>Username</td><td><input type="text" name="username" v-model="user.username"></td></tr>
-			<tr><td>Password</td><td><input type="password" name="password" v-model="user.password"></td></tr>
+			<tr><td>Username</td><td><input type="text" v-model="user.username"></td></tr>
+			<tr><td>Password</td><td><input type="password" v-model="user.password"></td></tr>
 			<tr><td><input type="submit" value="Login" v-on:click = "checkUser"></td></tr>
 		</table>
 	</form>
@@ -21,8 +21,8 @@ Vue.component("login", {
     	checkUser : function() {
 			//this.user.username = username.value
 			//this.user.password = password.value
-			axios.post('rest/login/', this.user).
 			//axios.post('rest/login' + this.user.username + ',' + this.user.password).
+			axios.post('rest/login/', this.user).
 				then(response => (router.push(`/startup`)));
     	}
     }
--- WebShopREST/WebContent/components/startup.js
+++ WebShopREST/WebContent/components/startup.js
@@ -2,24 +2,18 @@ Vue.component("startup", {
 	data: function () {
 		    return {
 		      title: "Start",
-		      value: "Start",
+		      //value: "Start",
 		    }
 	},
 	template: ` 
 <div>
 	<p>Dobrodosli</p>
-	<form>
-		<table>
-			<tr><td>Username</td></tr>
-			<tr><td>Password</td></tr>
-		</table>
-	</form>
 </div>		  
     	`,
-    methods: {
-    	/*checkUser : function() {
+    /*methods: {
+    	checkUser : function() {
 			axios.post('rest/login', this.user).
 				then(response => (router.push(`/pocetna`)));
-    	}*/
-    }
+    	}
+    }*/
 });
