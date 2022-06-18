8dde8111d8900e1e6614ace3e7a87f693e6f2487
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2022-06-18 19:07:10 +0200

    Interfaec change

--- WebShopREST/WebContent/components/login.js
+++ WebShopREST/WebContent/components/login.js
@@ -9,13 +9,13 @@ Vue.component("login", {
 	},
 	template: ` 
 <div>
-	<body style="height:760px;background:url('pictures/naslovna.jpg');background-size:cover;margin:-7px">
+	<body style="height:760px;background:url('pictures/naslovna2.jpg');background-size:cover;margin:-7px">
 	<form>
-		<table style="position:relative;background-color: #FAD9E6; left:150px;top:200px;border-spacing:20px;width:280px;height:270px;border: 4px solid #f04331;padding-top: 70px;padding-bottom: 25px;padding-left:30px;padding-right:30px">
+		<table style="position:relative;background-color: #e7e7e5; left:150px;top:190px;border-spacing:25px;width:280px;height:270px;border: 5px solid #3e3e3e;padding-top: 70px;padding-bottom: 25px;padding-left:35px;padding-right:30px">
 			<tr><td>
-			<input type="text" placeholder="Username" v-model="user.username" style="width:220px;height:28px;font-size:20px;border:0px; background: none;border-bottom: 2px solid #f04331"></td></tr>
-			<tr><td><input type="password" placeholder="Password" v-model="user.password" style="width:220px;height:28px;font-size:20px;border:0px; background: none;border-bottom: 2px solid #f04331"></td></tr>
-			<tr><td><button value="Login" text="Login" v-on:click="checkUser" style="color:#F9F9F9;width:100%;height:35px;background-color: #f04331;font-size:20px">Login</button>
+			<input type="text" placeholder="Username" v-model="user.username" style="width:220px;height:28px;font-size:20px;border:0px; background: none;border-bottom: 2px solid #3e3e3e"></td></tr>
+			<tr><td><input type="password" placeholder="Password" v-model="user.password" style="width:220px;height:28px;font-size:20px;border:0px; background: none;border-bottom: 2px solid #3e3e3e"></td></tr>
+			<tr><td><button value="Login" text="Login" v-on:click="checkUser" style="color:#F9F9F9;width:100%;height:40px;background-color: #3e3e3e;font-size:20px;border:none">Login</button>
 			<p>Don't have account?  <a v-on:click ="Register"><u><span style="cursor:pointer">Register here</span></u></a></p>
 			<p id="error">{{error}}</p></td></tr>
 		</table>
