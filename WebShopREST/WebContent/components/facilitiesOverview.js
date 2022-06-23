805ed63b8d51b43b3ac99a50b66f0d3924ed735d
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2022-06-24 01:23:38 +0200

    Do nav bar

--- WebShopREST/WebContent/components/facilitiesOverview.js
+++ WebShopREST/WebContent/components/facilitiesOverview.js
@@ -13,15 +13,36 @@ var facilitiesOverview = new Vue({
 template: ` 
 <div>
 	<body>
-	<nav class="navbar navbar-expand-mb fixed-top navbar-light bg-light" style="background-color: #e7e7e5;font-size:50px; height: 70px">
-  		<div class="container-fluid" style="margin-bottom: 80px">
-			<a class="navbar-brand" style="margin-left: 20px">
+	<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light" style="background-color: #e7e7e5;font-size:50px; height: 70px;">
+  		<div class="container-fluid" style="margin-bottom: 0px">
+			<a class="navbar-brand" style="margin-left: 20px;margin-right: 120px; font-size: 28px">
       			<img src="pictures/barbell-2.png" alt="" width="65" height="55" style="margin-right: 10px" class="d-inline-block">
       			Fitpass
     		</a>  		
-    	</div>
+    	
+	    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse d-flex flex-row-reverse gap-2" id="navbarNav" style="font-size: 20px">
+		      <ul class="navbar-nav d-flex gap-2">
+		        <li class="nav-item">
+		          <a class="nav-link active" aria-current="page" href="#">Pocetna</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#">Treninzi</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" v-on:click="LogOut" href="#">Profil</a>
+		        </li>
+		      </ul>
+		    </div>
+		    
+		    <form class="d-flex" style="margin-left: 30px">
+	      		<button class="btn btn-outline-secondary" v-on:click="LogOut" type="submit">Log out</button>
+	    	</form>
+	    </div>
 	</nav>
-	<section>
+	<section id="intro">
 	<h1 style="margin-top: 120px">Our facilities:</h1>
 	<form style="font-size:20px" >
 		<label>Search by:</label>
@@ -89,6 +110,11 @@ template: `
     	},
     	resetSearch : function(){
 			this.facilitiesToShow = this.allFacilities;	
+		},
+		LogOut : function(){
+			event.preventDefault();
+			//router.push(`/startup`);
+			window.location.href = 'products.html';
 		},
 		propertyToSearchBySelectionChanged : function(event){
 			this.propToSearchBy = event.target.value;
