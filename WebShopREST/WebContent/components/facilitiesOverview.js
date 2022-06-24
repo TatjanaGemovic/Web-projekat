f9e088d11e4b854dcc36524a2820e966ff660453
FStefanovv <filipficastefanov22@gmail.com>
2022-06-24 02:35:33 +0200

    improvements in the styling of facilities overview

--- WebShopREST/WebContent/components/facilitiesOverview.js
+++ WebShopREST/WebContent/components/facilitiesOverview.js
@@ -12,29 +12,30 @@ var facilitiesOverview = new Vue({
 	},
 template: ` 
 <div>
-	<h1>Our facilities:</h1>
-	<form style="font-size:20px" >
+	<h2 style="margin-top:16%; margin-bottom:5%" class="text-center">Our facilities:</h2>
+	<div class="row justify-content-center">
+		<div class="col-lg-7 col-xs-4 col-sm-4 col-md-4"></div>
+		<form style="font-size:16px;margin-bottom:3%" class="col-lg-4 col-xs-7 col-sm-7 col-md-7">
 		<label>Search by:</label>
-		<select class="form-select form-select-sm" v-on:change="propertyToSearchBySelectionChanged($event)">
+		<select class="form-select form-select-sm" v-on:change="propertyToSearchBySelectionChanged($event)" :style="{ 'width': '80%'}">
   			<option selected value="0">Name</option>
   			<option value="1">Type</option>
   			<option value="2">Location</option>
   			<option value="3">Minimum rating</option>
 		</select>
-		<input type="text" v-if="inputTextNeeded" v-model="textInputValueToSearchBy" name="searchInput" placeholder="Type here..."/>
-		<select class="form-select form-select-sm" v-else  v-on:change="facilityTypeSelectionChanged($event)">
+		<input type="text" class="form-control-sm" v-if="inputTextNeeded" v-model="textInputValueToSearchBy" name="searchInput" placeholder="Type here..." :style="{ 'width': '80%'}">
+		<select class="form-select form-select-sm" v-else  v-on:change="facilityTypeSelectionChanged($event)" :style="{ 'width': '80%'}">
   			<option selected value="GYM" v-model="searchedFacilityType">Gym</option>
   			<option value="POOL" v-model="searchedFacilityType">Pool</option>
   			<option value="SPORTS_CENTRE" v-model="searchedFacilityType">Sports center</option>
   			<option value="DANCE_STUDIO" v-model="searchedFacilityType">Dance studio</option>
 		</select>
-    	 
-    	<button class="btn btn-primary" v-on:click="search">Search</button>
-    	<button class="btn btn-secondary" v-on:click="resetSearch">Reset search</button>
+    	 <br>
+    	<button class="btn btn-primary btn-sm" v-on:click="search">Search</button>
+    	<button class="btn btn-secondary btn-sm" v-on:click="resetSearch">Reset search</button>
 	</form>
-	<br>
-	<br>
-	<div class="row justify-content-center">
+	<div class="col-1"></div>
+	
 		<div v-for="facility in facilitiesToShow" class="col-md-3 card m-3"> 
 			<img v-bind:src="facility.imageURI" class="card-img-top pt-2" /> 
 			<div class="card-body">
@@ -42,7 +43,6 @@ template: `
 				<p class="card-text ps-2">Rating: {{parseFloat(facility.rating).toFixed(1)}}/5.0</p>
 			</div>
 		</div>
-	</div>
 </div>		  
     	`,
     mounted() {
